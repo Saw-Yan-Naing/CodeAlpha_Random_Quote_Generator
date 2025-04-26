097aaafb91e12e8a9847fd79365562c2f98d6f8a
R12 <106422358+Saw-Yan-Naing@users.noreply.github.com>
2025-04-26 07:30:21 +0630

    share function into provider

--- lib/quote_generator/presentation/quote_generator_page.dart
+++ lib/quote_generator/presentation/quote_generator_page.dart
@@ -1,17 +1,8 @@
-import 'dart:io';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:random_quote_generator/core/error/app_error_catching.dart';
-import 'package:random_quote_generator/core/error/error_provider.dart';
 import 'package:random_quote_generator/quote_generator/domain/assets_image.dart';
 import 'package:random_quote_generator/quote_generator/presentation/quote_card.dart';
-import 'dart:ui' as ui;
-import 'package:share_plus/share_plus.dart';
 
 import '../provider/quote_provider.dart';
 
@@ -26,46 +17,10 @@ class QuoteGeneratorPage extends ConsumerStatefulWidget {
 class _QuoteGeneratorPageState extends ConsumerState<QuoteGeneratorPage> {
   final GlobalKey _shareKey = GlobalKey();
 
-  Future<void> _shareQuote() async {
-    try {
-      RenderRepaintBoundary boundary =
-          _shareKey.currentContext!.findRenderObject() as RenderRepaintBoundary;
-      ui.Image image = await boundary.toImage();
-      ByteData? byteData = await image.toByteData(
-        format: ui.ImageByteFormat.png,
-      );
-      final pngBytes = byteData!.buffer.asUint8List();
-
-      final tempDir = await getTemporaryDirectory();
-      final file = await File('${tempDir.path}/quote.png').create();
-      await file.writeAsBytes(pngBytes);
-
-      final shareResult = await SharePlus.instance.share(
-        ShareParams(
-          text: 'Check out this quote!',
-          files: [XFile(file.path)],
-          subject: 'Quote of the Day',
-        ),
-      );
-
-      debugPrint('Share result: ${shareResult.status} ${shareResult.raw}');
-    } catch (e) {
-      ref
-          .read(errorProvider)
-          .showError(
-            AppError(
-              message: e.toString(),
-              stackTrace: StackTrace.current,
-              errorDisplayType: ErrorDisplayType.dialog,
-            ),
-          );
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     final quote = ref.watch(quoteGenerateProvider);
-
+    final func = ref.read(quoteGenerateProvider.notifier);
     return SafeArea(
       child: Scaffold(
         body: DecoratedBox(
@@ -129,7 +84,9 @@ class _QuoteGeneratorPageState extends ConsumerState<QuoteGeneratorPage> {
                                   nextQuote: () {
                                     ref.invalidate(quoteGenerateProvider);
                                   },
-                                  shareQuote: _shareQuote,
+                                  shareQuote:
+                                      () =>
+                                          func.shareQuote(shareKey: _shareKey),
                                   isLoading: quote.isLoading,
                                 ),
                               ),
--- lib/quote_generator/provider/quote_provider.dart
+++ lib/quote_generator/provider/quote_provider.dart
@@ -1,8 +1,15 @@
+import 'dart:io';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
 import 'dart:async';
 
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:random_quote_generator/core/error/error_provider.dart';
 import 'package:random_quote_generator/quote_generator/domain/quote_model.dart';
+import 'package:share_plus/share_plus.dart';
 
 import '../../core/error/app_error_catching.dart';
 import '../data/quote_repository.dart';
@@ -27,6 +34,43 @@ class QuoteGenerateProvider extends AsyncNotifier<QuoteModel?> {
     }
 
     return quote.data;
+  }
+
+  Future<void> shareQuote({required GlobalKey shareKey}) async {
+    try {
+      RenderRepaintBoundary boundary =
+          shareKey.currentContext!.findRenderObject() as RenderRepaintBoundary;
+
+      ui.Image image = await boundary.toImage(pixelRatio: 3.0);
+      ByteData? byteData = await image.toByteData(
+        format: ui.ImageByteFormat.png,
+      );
+      final pngBytes = byteData!.buffer.asUint8List();
+
+      final tempDir = await getTemporaryDirectory();
+      final file = await File('${tempDir.path}/quote.png').create();
+      await file.writeAsBytes(pngBytes);
+
+      final shareResult = await SharePlus.instance.share(
+        ShareParams(
+          text: 'Check out this quote!',
+          files: [XFile(file.path)],
+          subject: 'Quote of the Day',
+        ),
+      );
+
+      debugPrint('Share result: ${shareResult.status} ${shareResult.raw}');
+    } catch (e) {
+      ref
+          .read(errorProvider)
+          .showError(
+            AppError(
+              message: e.toString(),
+              stackTrace: StackTrace.current,
+              errorDisplayType: ErrorDisplayType.dialog,
+            ),
+          );
+    }
   }
 }
 
